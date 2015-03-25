e37203d5c2194445e8a2661f8a92fe545292b814
N3962 <n3962@N3962ui-Mac-mini.local>
2015-03-25 11:21:47 +0900

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by N3962 on 2015. 3. 25..
+//  Copyright (c) 2015년 N3962. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
